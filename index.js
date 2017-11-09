19f8b80cc0c093a10ad5921d4e5e65537ff4c338
Dustin Wang <dustinrwang@gmail.com>
2017-11-09 08:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ function distanceFromHqInFeet(location) {
 
 function distanceTravelledInFeet(start, end) {
   let distance;
-(end - start>0) ? distance=end-start : distance=start-end;  
+(end - start>0) ? distance=end-start : distance=start-end;
     return distance * 264;
   }
+
+function calculatesFarePrice(start, end) {
+  let cost=distanceTravelledInFeet(start, end);
+  if (cost<400) {
+    return 0;
+  } else if (cost>400 && cost<2000) {
+    return cost * 0.02;
+  } else if (cost>2000 && cost<2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
